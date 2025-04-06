36c176b21e2a3457b05464aafad5cc62f5d8db44
Alekhyagurram11 <158799377+Alekhyagurram11@users.noreply.github.com>
2025-04-06 19:25:54 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'TDUXnY_tb2xO1i3jdhT_w0Y4wQQzDyOHsH8VzIbbEBPBNth5TBJgiSTY5nAgdQYdaIw'  # Replace with a secure key
+SECRET_KEY = ''  # Replace with a secure key
 DEBUG = True
 ALLOWED_HOSTS = []
 
